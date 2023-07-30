aa067c5f0123fede9f405e41d27c12e98060b52d
Süleyman Çakır <scakir1978@gmail.com>
2023-07-30 20:03:45 +0300

    IEntity update

--- Shared/BOnlineStore.Shared/Entity/IEntity.cs
+++ Shared/BOnlineStore.Shared/Entity/IEntity.cs
@@ -3,16 +3,11 @@ using MongoDB.Bson.Serialization.Attributes;
 
 namespace BOnlineStore.Shared.Entities
 {
-    public interface IEntity
+    public interface IEntity : ITenant
     {
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
         string Id { get; }
 
-        [BsonRepresentation(BsonType.String)]
-        Guid TenantId { get; }
-
-        void SetTenant(Guid tenantId);
-        
     }
 }
---
+++ Shared/BOnlineStore.Shared/Entity/ITenant.cs
@@ -0,0 +1,13 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace BOnlineStore.Shared.Entities
+{
+    public interface ITenant
+    {
+        [BsonRepresentation(BsonType.String)]
+        Guid TenantId { get; }
+
+        void SetTenant(Guid tenantId);
+    }
+}
