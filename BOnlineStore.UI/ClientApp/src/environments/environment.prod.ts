4bfdb8932e5615ad448942b199f2881b50933a77
Süleyman Çakır <scakir1978@gmail.com>
2025-10-22 11:45:06 +0300

    Alan adı URL'lerini b-online-store.com olarak güncelleme
    Yapılandırma dosyalarındaki URL'ler, yeni alan adı yapısını yansıtacak şekilde bonlinestore.com'dan b-online-store.com'a değiştirilerek güncellendi. Bu, kimlik (identity), tanımlar (definitions), üretim (production), kullanıcı arayüzü (UI), BFF ve diğer servisler için environment.prod.ts, deployment.yaml, config-map.yaml, routing.yaml ve docker-compose.override.yml dosyalarındaki güncellemeleri içermektedir. Tüm yapılandırmaların yeni alan adını işaret etmesi sağlandı.

--- BOnlineStore.UI/ClientApp/src/environments/environment.prod.ts
+++ BOnlineStore.UI/ClientApp/src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: true,
-  identityUrl: 'https://identity.bonlinestore.com',
-  definitionsUrl: 'https://definitions.bonlinestore.com/api/',
-  productionUrl: 'https://production.bonlinestore.com/api/',
-  //definitionsUrl: 'https://gateway.bonlinestore.com/services/definitions/',
-  //productionUrl: 'https://gateway.bonlinestore.com/services/production/',
-  uiUrl: 'https://ui.bonlinestore.com',
-  bffUrl: 'https://bff.bonlinestore.com/api/',
+  identityUrl: 'https://identity.b-online-store.com',
+  definitionsUrl: 'https://definitions.b-online-store.com/api/',
+  productionUrl: 'https://production.b-online-store.com/api/',
+  //definitionsUrl: 'https://gateway.b-online-store.com/services/definitions/',
+  //productionUrl: 'https://gateway.b-online-store.com/services/production/',
+  uiUrl: 'https://ui.b-online-store.com',
+  bffUrl: 'https://bff.b-online-store.com/api/',
 };
